70edcbdb3e50ae1631ddf8933c297845cb83464c
Philipp Verpoort <philipp.verpoort@pik-potsdam.de>
2025-07-07 23:15:36 +0200

    Deployed 2925e21 with MkDocs version: 1.6.1

---
+++ tutorials/R/R.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+devtools::load_all()
+
+# ## Raw file paths
+
+# The package contains definition files for the criteria. The paths to those files are contained in `file_paths`.
+
+# +
+library(scenariovettingcriteria)
+
+for (component_id in names(file_paths)) {
+  component_path <- file_paths[[component_id]]
+  print(paste0(component_id, ":  ...", substr(as.character(component_path), nchar(as.character(component_path)) - 39, nchar(as.character(component_path)))))
+}
+# -
+
+# ## Load functions
+
+# Instead of loading the data from these files manually, it is recommended to use the built-in load functions from the package via `load_criteria`. For instance, the following will load the definition of the thresholds values.
+
+load_criteria('criteria-thresholds')
